d8aa1717503986e9475e2b9fca0f796bc9fa9a9e
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-09 11:26:51 -0300

    Ajuste nos atributos e metodos das entidades

--- api/entities/account.py
+++ api/entities/account.py
@@ -2,10 +2,10 @@ from api.adapters.validators.document.interfaces.document_validator_interface im
 
 class Accout:
   
-  def __init__(self, document: str, user_name: str, value: float, validator: DocumentValidationInterface):
+  def __init__(self, document: str, user_name: str, balance: float, validator: DocumentValidationInterface):
     self._document = document
     self._user_name = user_name
-    self._value = value
+    self._balance = balance
     self.__validator = validator(document)
     self.__validator.validate()
 
@@ -18,5 +18,13 @@ class Accout:
     return self._user_name
 
   @property
-  def value(self):
-    return self._value
+  def balance(self):
+    return self._balance
+  
+  def cashin(self, value: float):
+    self._balance += value
+    
+  def cashout(self, value: float):
+    if value > self._balance:
+      raise Exception('Not enough cash')
+    self._balance -= value
