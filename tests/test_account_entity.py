7c3cb80b64147b8f298edbe2a7401c0fc4e19556
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-09 04:13:59 -0300

    add test

---
+++ tests/test_account_entity.py
@@ -0,0 +1,44 @@
+from api.entities.account import Accout
+from api.adapters.validators.document.document_validator import DocumentValidator
+
+def test_valid_document():
+  account = Accout(
+    document='12345678901', 
+    user_name='user', 
+    value=100.0,
+    validator=DocumentValidator
+  )
+  assert account.document == '12345678901' and account.user_name == 'user' and account.value == 100.0
+  
+def test_with_no_document():
+  try:
+    Accout(
+      document=None, 
+      user_name='user', 
+      value=100.0,
+      validator=DocumentValidator
+    )
+  except ValueError as error:
+    assert error.args[0] == 'Document is required'
+    
+def test_long_document():
+  try:
+    Accout(
+      document='123456789011', 
+      user_name='user', 
+      value=100.0,
+      validator=DocumentValidator
+    )
+  except ValueError as error:
+    assert error.args[0] == 'Document must have 11 characters'
+    
+def test_no_only_digits_document():
+  try:
+    Accout(
+      document='1234567890a', 
+      user_name='user', 
+      value=100.0,
+      validator=DocumentValidator
+    )
+  except ValueError as error:
+    assert error.args[0] == 'Document must have only numbers'
