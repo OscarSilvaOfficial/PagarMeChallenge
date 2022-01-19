f4db4c23a85a44cf9e58ff4eaac621afd98658c8
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-19 20:12:20 -0300

    Migração para mongo

--- api/infra/config/env.py
+++ api/infra/config/env.py
@@ -1,3 +1,3 @@
 import os
 
-NOSQL_DB_HOST = os.environ.get('NOSQL_DB_HOST', 'mongodb://127.0.0.1:27017/')
+NOSQL_DB_HOST = os.environ.get('NOSQL_DB_HOST', 'mongodb+srv://oscar:oscar@cluster0.xlh9q.gcp.mongodb.net/pagarme?retryWrites=true&w=majority')
